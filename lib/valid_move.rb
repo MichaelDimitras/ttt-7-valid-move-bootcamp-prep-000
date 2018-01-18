1aa048e23597bc453aa01e261214eb2972173629
Michael Dimitras <michaeladimitras@gmail.com>
2018-01-18 03:53:53 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,8 @@
 def valid_move?(board, idx)
 
   if(idx.between?(0,8))
-    return position_taken?(board, idx)
+    return true
+    #return position_taken?(board, idx)
   else
     return false
   end
